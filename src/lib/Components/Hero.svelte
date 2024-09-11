99ad6ddfc788de17491fe9db0706644e89dc1eb7
Andersson Beaz <andersson.beaz4313@gmail.com>
2024-09-10 22:43:34 -0600

    [feature]: Hero component

---
+++ src/lib/Components/Hero.svelte
@@ -0,0 +1,9 @@
+<script>
+    import "../../app.css"
+    import client from "$lib/images/clientLanding.jpg"
+</script>
+
+<h1 class="text-orange-300 font-bold text-6xl text-center">Aqui va la landing</h1>
+<img src={client} alt="">
+<h2>Hero xd</h2>
+<h3 class="flex items">random</h3>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-    import "../app.css"
+    import Hero from "$lib/Components/Hero.svelte";
+import "../app.css"
+    
 </script>
 
-<h1 class="text-orange-300 font-bold text-6xl text-center">Aqui va la landing</h1>
+<Hero />
