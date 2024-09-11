34858c2f25e1c3ca6ccf23420e1cad9b1e09f49f
Andersson Beaz <andersson.beaz4313@gmail.com>
2024-09-10 19:09:38 -0600

    initial commit

---
+++ src/routes/meeting/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import "../../app.css"
+</script>
+
+<h1 class="text-orange-300 font-bold text-6xl text-center">Aqui va el form</h1>
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "20px",
+          md: "50px"
+        }
+      },
+      fontFamily: {
+        //agregar fuente
+      },
+      colors: {
+        'gold-highlight' : "#BA7712",
+        'yellow-focus' : "#E6AF2E",
+        'blue-footer' : "#020F23",
+      },
+    }
+  },
+  plugins: []
+};
