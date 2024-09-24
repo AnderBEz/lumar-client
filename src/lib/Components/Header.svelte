453f09c93d998b9bc13c71a098024647295b0a87
Andersson Beaz <andersson.beaz4313@gmail.com>
2024-09-24 00:08:43 -0600

    [fix]: responsive header

--- src/lib/Components/Header.svelte
+++ src/lib/Components/Header.svelte
@@ -3,14 +3,14 @@
     import "../../routes/styles/images.css"
 </script>
 
-<header class="w-full lg:h-screen">
-    <div class="container py-36 lg:py-6 flex items-center lg:justify-start lg:pt-80">
-        <div class="lg:flex flex-col text-center lg:text-start pl-8 md:pl-0">
+<header class="w-full h-screen">
+    <div class="container py-36 lg:py-6 flex items-center justify-center lg:justify-start lg:pt-80">
+        <div class="flex flex-col text-center lg:text-start md:pl-0">
             <div class="text-center">
                 <h2 class="text-4xl lg:text-7xl text-white font-principal text-center">Barberia</h2>
                 <h1 class="text-6xl lg:text-9xl text-gold-highlight font-principal text-center">Lumar</h1>
             </div>
-            <div class="text-white text-center text-sm md:text-lg lg:text-2xl w-60 md:w-auto">
+            <div class="text-white text-center text-sm md:text-lg lg:text-2xl">
                 <p>Detras de un gran corte, existe una gran barbera.</p>
             </div>
             <div class="flex items-center justify-center py-3 text-center font-principal">
