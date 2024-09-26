a5f262ac9808ebacef4c9ad6076029376ac50998
Andersson Beaz <andersson.beaz4313@gmail.com>
2024-09-26 10:59:33 -0600

    [feature]: landing already

--- src/lib/Components/Information.svelte
+++ src/lib/Components/Information.svelte
@@ -5,8 +5,11 @@
 </script>
 
 <section>
-    <div class="container py-24">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <div class="container py-12">
+        <div class="text-center py-6">
+            <h2 class="text-3xl lg:text-6xl font-principal">Contactanos</h2>
+        </div>
+        <div class="py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <div class="flex space-x-3 items-center ">
                 <img src={cellIcon} alt="celularIcon" class="size-8 lg:size-12">
                 <h4 class="text-lg lg:text-xl font-semibold">+52 3321824126</h4>
