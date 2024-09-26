261e02dc2d2dcb5e2a0d0de80d92c6727d6c6b6f
Andersson Beaz <andersson.beaz4313@gmail.com>
2024-09-26 11:05:32 -0600

    finished

--- src/lib/Components/Location.svelte
+++ src/lib/Components/Location.svelte
@@ -4,6 +4,9 @@
 
 <section id="Ubicacion">
     <div class="container py-12">
+        <div class="text-center py-6">
+            <h2 class="py-6 text-3xl lg:text-6xl font-principal text-white">Visitanos!</h2>
+        </div>
         <div class="flex items-center justify-center h-full w-auto">
             <iframe title="Mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3736.361530271779!2d-103.18556322498274!3d20.53238358099561!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842f4a7b7fd23b71%3A0xc868bfd8ff1858ef!2sVilla%20Durango%2C%2045680%20El%20Salto%2C%20Jal.!5e0!3m2!1ses-419!2smx!4v1727154437575!5m2!1ses-419!2smx" width="600px" height="450px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
